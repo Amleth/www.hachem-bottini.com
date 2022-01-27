ddea6270c276257dceb6c6483165e59968c044bb
Thomas ⬡ Bottini <thomas.bottini@gmail.com>
2022-01-27 14:44:23 +0100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,27 @@
 module.exports = {
     siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+        siteUrl: `https://www.hachem-bottini.com`,
+        title: `Hachem ⬡ Bottini`
     },
     plugins: [
-
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `diptyques`,
+                path: `${__dirname}/src/data/diptyques/`,
+            },
+        },
+        `gatsby-plugin-image`,
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-mdx`,
+        {
+            resolve: 'gatsby-plugin-web-font-loader',
+            options: {
+                google: {
+                    families: ['Jost']
+                }
+            }
+        }
     ]
 }
